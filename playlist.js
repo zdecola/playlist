adb7a90bf6fa17e82fb523d725a6314035f83375
Peter <pwjablonski@gmail.com>
2017-07-12 09:48:01 -0400

    add to github

---
+++ playlist.js
@@ -0,0 +1,50 @@
+var mySong = {
+	"title":"24K Magic",
+	"artist":"Bruno Mars",
+	"length":"3:15",
+	"release-date": "March 15th, 2017",
+	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"genres":[
+		"pop",
+		"R&B"
+	]
+}
+
+var myPlayList = [
+	{
+		"title":"24K Magic",
+		"artist":"Bruno Mars",
+		"length":"3:15",
+		"release-date": "March 15th, 2017",
+		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"genres":[
+			"pop",
+			"R&B"
+		]
+	},
+	{
+		"title":"Pony",
+		"artist":"Genuwine",
+		"length":"5:25",
+		"release-date":"August 20, 1996",
+		"mp3-url":"https://open.spotify.com/track/6mz1fBdKATx6qP4oP1I65G",
+		"image-url":"https://upload.wikimedia.org/wikipedia/en/3/37/Ginuwine_-_Pony.JPG",
+		"genres":[
+			"R&B"
+		]
+	},
+	{
+		"title":"Sorry",
+		"artist":"Justin Bieber",
+		"length":"3:20",
+		"release-date":"",
+		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"genres":[
+			"pop",
+		]
+	}
+
+]
---
+++ script.js
@@ -0,0 +1,25 @@
+// Class 2:
+// Create a loop that enumerates through mySong variable
+// Use jQuery and string concatenation to display data for this single song on the page
+
+// Class 3:
+// Create a loop that iterates through the myPlaylist variable
+// Use jQuery and string concatenation to display data for multiple songs on the page
+
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+
+//GLOBAL VARIABLES
+
+
+
+//FUNCTIONS
+
+
+
+// DOCUMENT READY FUNCTION
+
+
+
+
