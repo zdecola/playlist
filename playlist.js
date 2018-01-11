95a49304a56c5c49d9c95e661525f3889b8dcf39
Peter Jablonski <pwjablonski@gmail.com>
2018-01-11 15:50:51 -0500

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,51 +1,28 @@
 var mySong = {
 	"title":"24K Magic",
 	"artist":"Bruno Mars",
-	"length":"3:15",
-	"release-date": "March 15th, 2017",
 	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
 	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-	"genres":[
-		"pop",
-		"R&B"
-	]
 }
 
 var myPlayList = [
 	{
 		"title":"24K Magic",
 		"artist":"Bruno Mars",
-		"length":"3:15",
-		"release-date": "March 15th, 2017",
 		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
 		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-		"genres":[
-			"pop",
-			"R&B"
-		]
 	},
 	{
 		"title":"Sir Duke",
 		"artist":"Stevie Wonder",
-		"length":"3:52",
-		"release-date":"September 28, 1976",
 		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
 		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
-		"genres":[
-			"R&B",
-			"Funk",
-		]
 	},
 	{
 		"title":"Sorry",
 		"artist":"Justin Bieber",
-		"length":"3:20",
-		"release-date":"",
 		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
 		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
-		"genres":[
-			"pop",
-		]
 	}
 
 ]
