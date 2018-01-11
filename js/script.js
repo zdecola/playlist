e0de67e2a7960f89d6c4c488f4eb785901447c5f
Peter Jablonski <pwjablonski@gmail.com>
2018-01-11 15:59:20 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 // Class 2:
-// Create a loop that enumerates through mySong variable
-// Use jQuery and string concatenation to display data for this single song on the page
-
-// Class 3:
 // Create a loop that iterates through the myPlaylist variable
 // Use jQuery and string concatenation to display data for multiple songs on the page
+
+// Class 3:
+// Use input boxes to add songs to the playlist array. 
+
+
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-
-
-
-//FUNCTIONS
-
 
 
 // DOCUMENT READY FUNCTION
