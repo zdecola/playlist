5de4a23a67ec74fc5846897aee0e2f89bb599217
Zoelie DeCola <zdecola@cristoreyny.org>
2018-03-23 12:56:33 +0000

    html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// quick fix for Cloud9 warning:
+        // quick fix for Cloud9 warning:
 /* global $ */
 
 // Class 2:
@@ -17,30 +17,30 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"title":"El Clavo",
+	"artist":"Prince Royce",
+	"mp3-url":"",
+	"image-url":,
 }
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"title":"God's Plan",
+		"artist":"Drake",
+		"mp3-url":" ",
+		"image-url":"https://i2.wp.com/revealyourtruth.com/wp-content/uploads/2018/03/b25c24e950fa429f85466b59f8cbffa5.707e1.png?fit=1200%2C630&ssl=1&resize=350%2C200",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"The Middle",
+		"artist":"Zedd, Maren Morris, Grey",
+		"mp3-url":" ",
+		"image-url":" ",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"IDGAF",
+		"artist":"Dua Lipa",
+		"mp3-url":" ",
+		"image-url":" ",
 	}
 
 ]
